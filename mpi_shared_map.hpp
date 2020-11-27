b991666cbdea34222a35dd1867687ed9f61a1e09
Hikaru Takayashiki <you@example.com>
2020-11-27 17:01:41 +0900

    fixed specialization of template should be in .cpp

--- mpi_shared_map.hpp
+++ mpi_shared_map.hpp
@@ -72,46 +72,5 @@ MPISharedMap<TYPE>::copy_map( TYPE* cmap )
   for( int i = 0; i < size; i++ ) cmap[i] = map[i];
 }
 
-//specialized for long long int
-
-template<>
-void
-MPISharedMap<long long int>::mpi_bcast_from( int root_rank )
-{
-  MPI_Bcast( map, size, MPI_LONG_LONG_INT, root_rank, MPI_COMM_WORLD );
-}
-
-template<>
-void
-MPISharedMap<long long int>::mpi_send_to_recv_from( int dest, int source )
-{
-  assert(dest != source);
-  if ( myrank == source )
-    MPI_Send( map, size, MPI_LONG_LONG_INT, dest, 0, MPI_COMM_WORLD );
-  else if ( myrank == dest )
-    MPI_Recv( map, size, MPI_LONG_LONG_INT, source, 0, MPI_COMM_WORLD, &stat );
-}
-
-//specialized for int
-
-template<>
-void
-MPISharedMap<int>::mpi_bcast_from( int root_rank )
-{
-  MPI_Bcast( map, size, MPI_INT, root_rank, MPI_COMM_WORLD );
-}
-
-template<>
-void
-MPISharedMap<int>::mpi_send_to_recv_from( int dest, int source )
-{
-  assert(dest != source);
-  if ( myrank == source )
-    MPI_Send( map, size, MPI_INT, dest, 0, MPI_COMM_WORLD );
-  else if ( myrank == dest )
-    MPI_Recv( map, size, MPI_INT, source, 0, MPI_COMM_WORLD, &stat );
-}
-
-
 
 #endif
