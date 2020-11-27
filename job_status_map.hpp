e26e1860adcebae9bea07c8bd9622c2bc1e37b50
Hikaru Takayashiki <you@example.com>
2020-11-28 03:15:07 +0900

    add bcast integrity check

--- job_status_map.hpp
+++ job_status_map.hpp
@@ -8,50 +8,60 @@
 
 class JobStatusMap
 {
-  MPISharedMap<int> wait_jobs;
-  MPISharedMap<int> exec_jobs;
-  MPISharedMap<int> exit_jobs;
-  MPISharedMap<TIME_T> start_time;
-  MPISharedMap<TIME_T> end_time;
-  int size;
-  
-public:
-  JobStatusMap( int _myrank, int _size );
-  
-  void mpi_bcast_from( int root_rank ) {
-    wait_jobs.mpi_bcast_from(root_rank);
-    exec_jobs.mpi_bcast_from(root_rank);
-    exit_jobs.mpi_bcast_from(root_rank);
-    start_time.mpi_bcast_from(root_rank);
-    end_time.mpi_bcast_from(root_rank);
-  }
+    MPISharedMap<int> wait_jobs;
+    MPISharedMap<int> exec_jobs;
+    MPISharedMap<int> exit_jobs;
+    MPISharedMap<TIME_T> start_time;
+    MPISharedMap<TIME_T> end_time;
+    int size;
 
-  void mpi_send_to_recv_from( int dest, int source ) {
-    wait_jobs.mpi_send_to_recv_from( dest, source );
-    exec_jobs.mpi_send_to_recv_from( dest, source );
-    exit_jobs.mpi_send_to_recv_from( dest, source );
-    start_time.mpi_send_to_recv_from( dest, source );
-    end_time.mpi_send_to_recv_from( dest, source );
-  }
-  
-  bool existWaitJobs() {
-    for( int i = 0; i < size; i++ )
-      if( wait_jobs[i] == TRUE )  
-        return true;
-    return false;
-  }
+    public:
+    JobStatusMap( int _myrank, int _size );
 
-  int getNextWaitJob() {
-    assert(existWaitJobs());
-    for( int i = 0; i < size; i++ )
-      if( wait_jobs[i] == TRUE ) {
-        return i;
-      }
-    std::exit(1);
-  }
+    void mpi_bcast_from( int root_rank ) {
+        start_time.mpi_bcast_from(root_rank);
+        MPI_Barrier(MPI_COMM_WORLD);
+        end_time.mpi_bcast_from(root_rank);
+        MPI_Barrier(MPI_COMM_WORLD);
+        wait_jobs.mpi_bcast_from(root_rank);
+        MPI_Barrier(MPI_COMM_WORLD);
+        exec_jobs.mpi_bcast_from(root_rank);
+        MPI_Barrier(MPI_COMM_WORLD);
+        exit_jobs.mpi_bcast_from(root_rank);
+        MPI_Barrier(MPI_COMM_WORLD);
 
-  void setExecuted( int i );
-  void setExit( int i );
+        for(int i=0; i < size; i++){
+            // integrity check
+            assert(wait_jobs[i]!=exec_jobs[i] || exec_jobs[i]!=exit_jobs[i]);
+        }
+    }
 
-  void output_map(Command* cmd, ArgumentsList* arglist);
+    void mpi_send_to_recv_from( int dest, int source ) {
+        wait_jobs.mpi_send_to_recv_from( dest, source );
+        exec_jobs.mpi_send_to_recv_from( dest, source );
+        exit_jobs.mpi_send_to_recv_from( dest, source );
+        start_time.mpi_send_to_recv_from( dest, source );
+        end_time.mpi_send_to_recv_from( dest, source );
+    }
+
+    bool existWaitJobs() {
+        for( int i = 0; i < size; i++ )
+            if( wait_jobs[i] == TRUE )  
+                return true;
+        return false;
+    }
+
+    int getNextWaitJob() {
+        assert(existWaitJobs());
+        for( int i = 0; i < size; i++ )
+            if( wait_jobs[i] == TRUE ) {
+                return i;
+            }
+        std::exit(1);
+    }
+
+    void setExecuted( int i );
+    void setExit( int i );
+
+    void output_map(Command* cmd, ArgumentsList* arglist);
 };
