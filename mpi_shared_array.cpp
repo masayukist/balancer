675967922ee5d426baed40fc809a1695b514a81c
Hikaru Takayashiki <you@example.com>
2020-12-04 21:37:46 +0900

    add specialization for char

--- mpi_shared_array.cpp
+++ mpi_shared_array.cpp
@@ -58,3 +58,32 @@ MPISharedArray<int>::mpi_send_to_recv_from( int dest, int source )
     assert(ret == MPI_SUCCESS);
   }
 }
+
+//specialized for char
+template<>
+void
+MPISharedArray<char>::mpi_bcast_from( int root_rank )
+{
+    MPI_Win_fence(0,wintable);
+
+    auto ret = MPI_Bcast( map, size, MPI_CHAR, root_rank, MPI_COMM_WORLD );
+    assert(ret == MPI_SUCCESS);
+
+    MPI_Win_fence(0,wintable);
+}
+
+template<>
+void
+MPISharedArray<char>::mpi_send_to_recv_from( int dest, int source )
+{
+  if (dest == source) return;
+
+  assert(dest != source);
+  if ( myrank == source ){
+    auto ret = MPI_Send( map, size, MPI_CHAR, dest, 0, MPI_COMM_WORLD );
+    assert(ret == MPI_SUCCESS);
+  }else if ( myrank == dest ){
+    auto ret = MPI_Recv( map, size, MPI_CHAR, source, 0, MPI_COMM_WORLD, &stat );
+    assert(ret == MPI_SUCCESS);
+  }
+}
